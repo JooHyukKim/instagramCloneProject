a40b00ebace06589dfcebbe7c65bf77331df3d16
박 찬영 <cksdud7158@gmail.com>
2020-05-14 10:00:19 +0900

    Add + 예외 일부 추가

--- InstagramProject/instagram/src/instagram/server/Database.java
+++ InstagramProject/instagram/src/instagram/server/Database.java
@@ -1,11 +1,14 @@
 package instagram.server;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import instagram.config.ServerInfo;
+import instagram.exception.DuplicateUserIdException;
 import instagram.vo.Comment;
 import instagram.vo.Hashtag;
 import instagram.vo.PersonTag;
@@ -14,28 +17,61 @@ import instagram.vo.User;
 
 public class Database implements DatabaseTemplate{
 
+	public Database(String serverIp) throws ClassNotFoundException{
+		Class.forName(ServerInfo.DRIVER_NAME);
+		System.out.println("드라이버 로딩 성공....");
+	}
+	
 	@Override
 	public Connection getConnect() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASS);
+		System.out.println("Database Connection");
+		return conn;
 	}
 
 	@Override
 	public void closeAll(PreparedStatement ps, Connection conn) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		if(ps!=null) ps.close();
+		if(conn!=null) conn.close();		
 	}
 
 	@Override
 	public void closeAll(ResultSet rs, PreparedStatement ps, Connection conn) throws SQLException {
-		// TODO Auto-generated method stub
+		if(rs!=null) rs.close();
+		closeAll(ps, conn);			
+	}
+	
+	public boolean isExist(String userId, Connection conn)throws SQLException { // 존재 유무 확인
+		String sql ="SELECT ssn FROM customer WHERE useId=?";
+		PreparedStatement ps = conn.prepareStatement(sql);
 		
+		ps.setString(1,userId);
+		ResultSet rs = ps.executeQuery();
+		return rs.next();
 	}
 
 	@Override
-	public void addUser(User user) throws SQLException {
-		// TODO Auto-generated method stub
+	public void addUser(User user) throws SQLException, DuplicateUserIdException {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+		conn =getConnect();
 		
+		if(!isExist(user.getUserId(), conn)) { // userId가 없다...
+			String query = "INSERT INTO user(userId, userName, password, email, gender) values(?,?,?,?,?)";
+			ps = conn.prepareStatement(query);
+			
+			ps.setString(1, user.getUserId());
+			ps.setString(2, user.getUserName());
+			ps.setString(2, user.getPassword());
+			ps.setString(4, user.getEmail()); // 생성자로 받을때 두개는 null로 받음
+			ps.setString(4, user.getGender());
+		}else { // 같은 userId 이미 존재
+			throw new DuplicateUserIdException(user.getUserId()+"는 이미 존재합니다.");
+		}
+		}finally {
+			closeAll(ps, conn);
+		}
 	}
 
 	@Override
